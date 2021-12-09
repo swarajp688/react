d7b61292d256d7a53c8fb8f391e33616a91fda2a
swarajp688 <swarajp688@gmail.com>
2021-12-09 12:21:12 +0530

    added increase quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,48 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            title : 'Phone',
+            price : 999,
+            qty : 1,
+            img : ''
+        }
+
+        
+    }
+    increaseQty = () =>{
+        console.log("test",this.state);
+    }
     render(){
+        const {price , title , qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-bock">
-                    <img style={styles.image}/>
+                    <img alt='Product' style={styles.image}/>
                 </div>
                 <div className="right-block" >
-                    <div style={{fontSize :35 }}>Phone</div>
-                    <div style={{color : "#777"}}>Rs 999</div>
-                    <div style={{color : "#777"}}>Qty </div>
+                    <div style={{fontSize :35 }}>{title}</div>
+                    <div style={{color : "#777"}}>Rs {price}</div>
+                    <div style={{color : "#777"}}>Qty: {qty} </div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt="Increase" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/992/992651.png" onClick={this.increaseQty}>
+                        </img>
+                        <img alt="Decrease" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/992/992683.png">
+
+                        </img>
+                        <img alt="Delete" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png">
+
+                        </img>
+
                     </div>
                 </div>
             </div>
